f47ea7d3196f97bead177b8cc6efffb67a34cf21
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-18 16:46:13 +0530

    Promises

--- 11Aug_oop's.js
+++ 11Aug_oop's.js
@@ -48,17 +48,34 @@
 // vasanth.setFullName = "Rajesh";
 // console.log(vasanth.fullName);
 
-Person();
-class Person1 {
-  constructor() {
-    console.log("person in class");
-  }
-}
+// Person();
+// class Person1 {
+//   constructor() {
+//     console.log("person in class");
+//   }
+// }
 
-new Person1();
+// new Person1();
 
-function Person() {
-  console.log("person in fn ");
-}
+// function Person() {
+//   console.log("person in fn ");
+// }
 
 // function declarations are hoisted but classes are not hoisted
+
+function Vehicle(name,maker,engine){
+  this.name = name,
+  this.maker = maker,
+  this.engine = engine
+};
+
+Vehicle.prototype.getDetails = function(){
+  console.log('The name of the bike is '+ this.name);
+}
+
+let bike1 = new Vehicle('Hayabusa','Suzuki','1340cc');
+let bike2 = new Vehicle('Ninja','Kawasaki','998cc');
+
+console.log(bike1.name);
+console.log(bike2.maker);
+console.log(bike1.getDetails());
---
+++ 17Aug_Promises.js
@@ -0,0 +1,11 @@
+Promise.resolve('Success!')
+  .then(() => {
+    throw Error('Oh noes!')
+  })
+  .catch(error => {
+    return 'actually, that worked'
+  })
+  .then(data => {
+    throw Error('The fails!')
+  })
+  .catch(error => console.log(error.message))
