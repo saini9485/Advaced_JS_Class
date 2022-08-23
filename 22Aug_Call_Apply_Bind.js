39600051bb222502413847e0eef0d932ce0e0019
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-23 08:18:42 +0530

    call apply bind

---
+++ 22Aug_Call_Apply_Bind.js
@@ -0,0 +1,65 @@
+// let vasanth = {
+//   name: "vasanth",
+//   age: 12,
+// };
+
+// let getData = function (place, char = "joyful") {
+//   console.log(
+//     `${this.name} is ${this.age} years old. I'm from ${place}. I'm ${char}`
+//   );
+// };
+
+// let Rajesh = {
+//   name: "Rajesh",
+//   age: 21,
+// };
+
+// function borrowing -> call, apply
+// getData.call(Rajehs, "Lucknow", "notorious");
+// getData.call(vasanth, "Chennai");
+
+// getData.apply(Rajesh, ["Lucknow", "notorious"]);
+
+// let persons = [
+//   {
+//     obj: {
+//       name: "Rajesh",
+//       age: 21,
+//     },
+//     params: ["Lucknow", "notorious"],
+//   },
+//   {
+//     obj: {
+//       name: "vasanth",
+//       age: 12,
+//     },
+//     params: ["chennai", "cool"],
+//   },
+// ];
+
+// for (let data of persons) {
+//   //   console.log(data.obj, data.params);
+//   getData.apply(data.obj, data.params);
+// }
+
+// let vasanth = {
+//   name: "vasanth",
+//   age: 12,
+// };
+
+// let getData = function (place, char = "joyful") {
+//   console.log(
+//     `${this.name} is ${this.age} years old. I'm from ${place}. I'm ${char}`
+//   );
+// };
+
+// let Rajesh = {
+//   name: "Rajesh",
+//   age: 21,
+// };
+
+// // bind
+let RajeshData = getData.bind(Rajesh, "Lucknow");
+let vasanthData = getData.bind(vasanth, "chennai", "cool");
+// sameerData();
+vasanthData();
