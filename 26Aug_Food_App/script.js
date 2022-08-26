7c269ade2171ff88949db5d1b8efd491d473fe0b
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-26 22:15:07 +0530

    Food

--- 26Aug_Food_App/script.js
+++ 26Aug_Food_App/script.js
@@ -1,26 +1,21 @@
- //query selectors
- var input = document.querySelector('.search');
+var input = document.querySelector('.search');
  var btn1 = document.querySelector('.submitButton');
  let container = document.querySelector(".container")
  btn1.addEventListener('click',function(event){
-     // console.log(name.value)
      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)//fetching api data
      .then(response=>response.json())//text --> json
      .then(data=>{
          container.innerHTML = ""
          let food=data['meals']
-         //console.log(data);
          food.forEach(ele=>  {
-
              let {strMeal,strMealThumb,strInstructions}=ele
-
              let result= `<div>
-                 <h1>${strMeal}</h1>
+                 <h2>${strMeal}</h2>
                  <img src="${strMealThumb}" alt="" >
-                 <h2>${strInstructions}</h2>
+                 <h3>${strInstructions}</h3>
                  </div> `
              container.innerHTML += result
          })
-     }).catch(err=>alert("invalid  name"))
+     }).catch(err=>alert("Nothing found"))
 
  })
